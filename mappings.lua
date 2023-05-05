b590703d2def6d044e0f521b3af3d6989a62e48b
RilCritch <rilcritch@gmail.com>
2023-05-05 14:32:20 -0500

    Removed many mappings that I dont use

--- mappings.lua
+++ mappings.lua
@@ -1,18 +1,111 @@
 ---@type MappingsTable
 local M = {}
 
+M.disabled = { -- i can use these for other keybindings in future 
+  n = {
+    -- with leader ["<leader>keys"] = "",
+    ["<leader>n"] = "",
+    ["<leader>rn"] = "",
+    ["<leader>rh"] = "",
+    ["<leader>ph"] = "",
+    ["<leader>h"] = "",
+    ["<leader>v"] = "",
+    ["<leader>th"] = "",
+    ["<leader>td"] = "",
+
+    -- no modifier ["keys"] = "",
+    ["]c"] = "",
+    ["[c"] = "",
+
+    -- with ctrl ["<C-keys>"] = "",
+    ["<C-n>"] = "",
+
+    -- with alt ["<A-keys>"] = "",
+    ["<A-h>"] = "",
+    ["<A-v>"] = "",
+    ["<A-i>"] = "",
+  },
+
+  v = {
+
+  },
+
+  i = {
+
+  },
+
+  t = {
+    -- with alt ["<A-keys>"] = "",
+    ["<A-h>"] = "",
+    ["<A-v>"] = "",
+    ["<A-i>"] = "",
+  },
+}
+
 M.general = {
   n = {
-    -- naming
-    ["<leader>f"] = { name = "+find" },
     [";"] = { ":", "enter command mode", opts = { nowait = true } },
-    ["<leader>j"] = { "<cmd> HopWord <CR>", "HopWord" },
-    ["<leader>fk"] = { "<cmd> Telescope keymaps <CR>", "find keymaps" },
-    -- search-replace.nvim keybindings
 
   },
 }
 
+M.nvterm = {
+  n = {
+    ["<leader>t"] = {
+      function()
+        require("nvterm.terminal").toggle "float"
+      end,
+      "toggle floating term",
+    },
+    ["<A-t"] = {
+      function()
+        require("nvterm.terminal").toggle "float"
+      end,
+      "toggle floating term",
+    }
+  },
+
+  t = {
+    ["<A-t"] = {
+      function()
+        require("nvterm.terminal").toggle "float"
+      end,
+      "toggle floating term",
+    },
+  },
+}
+
+M.gitsigns = {
+  n = {
+    ["<leader>gd"] = {
+      function()
+        require("gitsigns").toggle_deleted()
+      end,
+      "Toggle deleted",
+    },
+  }
+}
+
+M.nvimtree = {
+  n = {
+    ["<leader>e"] = { "<cmd> NvimTreeToggle <CR>", "toggle nvimtree" },
+  },
+}
+
+M.navigation = {
+  n = {
+    ["<leader>j"] = { "<cmd> HopWord <CR>", "HopWord" },
+  },
+    -- search-replace.nvim keybindings
+}
+
+M.telescope = {
+  n = {
+    ["<leader>fk"] = { "<cmd> Telescope keymaps <CR>", "find keymaps" },
+    ["<leader>fz"] = { "<cmd> Telescope live_grep <CR>", "live grep" },
+    ["<leader>fw"] = { "<cmd> Telescope current_buffer_fuzzy_find <CR>", "find in current buffer" },
+  },
+}
 -- more keybinds!
 
 return M
