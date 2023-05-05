76888f87c9c176d2075c9a99b27d5b248546ea24
RilCritch <rilcritch@gmail.com>
2023-05-05 11:23:57 -0500

    tried to name the first command but it didnt work

--- mappings.lua
+++ mappings.lua
@@ -3,9 +3,13 @@ local M = {}
 
 M.general = {
   n = {
+    -- naming
+    -- ["<leader>f"] = { name = "find..." },
     [";"] = { ":", "enter command mode", opts = { nowait = true } },
     ["<leader>j"] = { "<cmd> HopWord <CR>", "HopWord" },
     ["<leader>fk"] = { "<cmd> Telescope keymaps <CR>", "find keymaps" },
+    -- search-replace.nvim keybindings
+
   },
 }
 
