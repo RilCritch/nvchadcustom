b91cb3c2105e74f9030ae96a1daba58c7476fbd2
RilCritch <rilcritch@gmail.com>
2023-06-23 17:56:26 -0500

    set up to run telescope find_files when empty

--- init.lua
+++ init.lua
@@ -9,6 +9,8 @@ local opt = vim.o
 --   pattern = "*",
 --   command = "tabdo wincmd =",
 -- })
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
 
 opt.relativenumber = true
 opt.scrolloff = 999 -- center cursor
@@ -25,3 +27,38 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   group = highlight_group,
   pattern = '*',
 })
+
+-- command to run when no file or directory is specified
+local function open_nvim_tree(data)
+
+  -- buffer is a real file on the disk
+  local real_file = vim.fn.filereadable(data.file) == 1
+
+  -- buffer is a [No Name]
+  local no_name = data.file == "" and vim.bo[data.buf].buftype == ""
+
+  -- buffer is a directory
+  -- local buftype = vim.api.nvim_buf_get_option(0, 'buftype')
+  -- local directory = vim.fn.isdirectory(data.file) == 1
+
+  -- if real_file or buftype == 'dir' and not no_name then
+  --   return
+  -- end
+  if real_file or not no_name then
+    return
+  end
+
+  -- nvim-tree
+  -- require("nvim-tree.api").tree.toggle({
+  --   focus = false,
+  --   find_file = true,
+  -- })
+
+  -- telescope
+  require('telescope.builtin').find_files({layout_strategy='vertical',layout_config={
+    preview_cutoff=999,
+    prompt_position='top'
+  }})
+end
+
+vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
