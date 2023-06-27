8198644696649f69d07a7d5ce63fcf35c5a82668
RilCritch <rilcritch@gmail.com>
2023-06-27 18:43:14 -0500

    added neorg

--- plugins.lua
+++ plugins.lua
@@ -127,6 +127,28 @@ local plugins = {
     end,
   },
 
+  {
+    "nvim-neorg/neorg",
+    build = ":Neorg sync-parsers",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("neorg").setup {
+        load = {
+          ["core.defaults"] = {}, -- Loads default behaviour
+          ["core.concealer"] = {}, -- Adds pretty icons to your documents
+          ["core.dirman"] = { -- Manages Neorg workspaces
+            config = {
+              workspaces = {
+                linux = "~/documents/notes/linux",
+                prog = "~/documents/notes/programming",
+              },
+            },
+          },
+        },
+      }
+    end,
+    lazy = false,
+  },
 }
 
 return plugins
